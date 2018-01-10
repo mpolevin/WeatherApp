32a41c37a879ae24ec8e150678d8f5dbd5477ec0
Mikhail Polyevin <mpolyevin@gmail.com>
2018-01-10 20:31:42 +0300

    Weather app

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -10,16 +10,67 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        searchBar.delegate = self
     }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+extension ViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        
+        let urlString = "https://api.apixu.com/v1/current.json?key=2fcbca7114dd43fb82e144949180601&q=\(searchBar.text!.replacingOccurrences(of: " ", with: "%20"))"
+        let url = URL(string: urlString)
+        
+        var locationName: String?
+        var temperature: Double?
+        var errorHasOccured: Bool = false
+        
+        let task = URLSession.shared.dataTask(with: url!) {[weak self] (data, response, error) in
+            do {
+                let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers)
+                    as! [String : AnyObject]
+                
+                if let _ = json["error"] {
+                    errorHasOccured = true
+                }
+                
+                if let location = json["location"] {
+                    locationName = location["name"] as? String
+                }
+                
+                if let current = json["current"] {
+                    temperature = current["temp_c"] as? Double
+                }
+                
+                DispatchQueue.main.async {
+                    if errorHasOccured {
+                        self?.cityLabel.text = "Error has occured"
+                        self?.temperatureLabel.isHidden = true
+                    }
+                    else {
+                        self?.cityLabel.text = locationName
+                        self?.temperatureLabel.text = "\(temperature!)"
+                        
+                        self?.temperatureLabel.isHidden = false
+                    }
+                }
+                
+            }
+            catch let jsonError {
+                print(jsonError)
+            }
+            
+        }
+        
+        task.resume()
+        
     }
-
-
 }
 
